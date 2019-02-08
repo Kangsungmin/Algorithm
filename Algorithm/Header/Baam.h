64bc21e6335aa2b78a54baaa9f721751a7596de8
Kangsungmin <belo7@naver.com>
2019-02-08 18:37:39 +0900

    Create Baam.h

---
+++ Algorithm/Header/Baam.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <vector>
+#include<iostream>
+class Baam
+{
+public:
+    Baam();
+    ~Baam();
+    void Input();
+    void Solve();
+    void DoSelf();
+private:
+
+};
+
+Baam::Baam()
+{
+}
+
+Baam::~Baam()
+{
+}
